dd4cedbb54820c3520bdda340b1e643c1668aaf5
Andy Pham <andy.pham111@gmail.com>
2020-10-12 16:25:58 -0700

    First Edit

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,36 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  MakeDancer.apply(this, arguments);
+  // this.$node = $('<span class="dancer"></span>');
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
+};
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
+MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
 
-  return blinkyDancer;
+// var oldStep = this.step();
+
+MakeBlinkyDancer.prototype.step = function() {
+  MakeDancer.prototype.step.call(this);
+  // call the old version of step at the beginning of any call to this new version of step
+
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+};
+
+var MakeMovingDancer = function(top, left, timeBetweenSteps) {
+  MakeDancer.apply(this, arguments);
+  this.location = 0;
+
+};
+
+MakeMovingDancer.prototype = Object.create(MakeDancer.prototype);
+MakeMovingDancer.prototype.constructor = MakeMovingDancer;
+
+MakeMovingDancer.prototype.moveNode = function() {
+
 };
--- src/init.js
+++ src/init.js
@@ -21,12 +21,16 @@ $(document).ready(function() {
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
+    if (dancerFunctionName === 'MakeBlinkyDancer') {
 
-    var dancer = dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
-      Math.random() * 1000
-    );
+
+      var dancer = new dancerMakerFunction(
+        $('body').height() * Math.random(),
+        $('body').width() * Math.random(),
+        Math.random() * 1000
+      );
+      window.dancers.push(dancer);
+    }
     $('body').append(dancer.$node);
   });
 });
